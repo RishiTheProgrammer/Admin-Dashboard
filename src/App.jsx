54773fc6bf20262c5435ba7dba8b9ccba41cebde
Rishi Soni <rishisprogramming@gmail.com>
2026-08-11 16:33:56 +0500

    Created the sturcture of dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Container, Row } from 'react-bootstrap'
+import Sidebar from './components/sidebar'
+import MainArea from './components/mainArea'
+import Navbar from './components/navbar'
 const App = () => {
   return (
     <>
-
+      <Navbar />
+    <Container fluid>
+      <Row>
+        <Sidebar />
+        <MainArea />
+      </Row>
+    </Container>
     </>
   )
 }
