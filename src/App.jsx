bf0de27736771aef6bf8b5d2615340fc999b8cfa
Rishi Soni <rishisprogramming@gmail.com>
2026-08-12 11:38:44 +0500

    Imported useState and useEffect and passed props to Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import { useState, useEffect } from 'react'
 import { Container, Row } from 'react-bootstrap'
 import Sidebar from './components/sidebar'
 import MainArea from './components/mainArea'
 import Navbar from './components/navbar'
 const App = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', theme);
+  }, [theme]);
+
   return (
-    <>
-      <Navbar />
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
     <Container fluid>
       <Row>
         <Sidebar />
         <MainArea />
       </Row>
     </Container>
-    </>
+    </div>
   )
 }
 
