c7252b9a9210882dc5e4dea4be7baa29f9c1f64a
Rishi Soni <rishisprogramming@gmail.com>
2026-08-14 20:11:11 +0500

    Used custom Logo instead of Admin Dashboard text

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { BsSpeedometer2 } from "react-icons/bs";
+import { TbHexagonLetterRFilled } from "react-icons/tb";
 import { TbCircleLetterRFilled } from "react-icons/tb";
 import { FaBell } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
@@ -23,8 +23,8 @@ const navbar = ({theme, setTheme}) => {
     <>
         <Navbar expand="lg" className="bg-body-tertiary m-2 rounded-2 px-2 py-2 navbar" fixed="top">
           <Container fluid>
-            <Navbar.Brand href="#" className='d-flex align-items-center gap-1'>
-              <BsSpeedometer2 className="me-2" /> Admin Dashboard
+            <Navbar.Brand href="https://rishitheprogrammer.vercel.app" className='d-flex align-items-center gap-1'>
+              <TbHexagonLetterRFilled size={30} className='me-2'/> Rishi
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
             <Navbar.Offcanvas
@@ -38,30 +38,31 @@ const navbar = ({theme, setTheme}) => {
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
-                <Nav className="mx-lg-auto d-lg-flex align-items-center gap-4">
+                <Nav className="mx-lg-auto mx-0 d-lg-flex align-items-center gap-4">
                     <NavLink to="/" end className={navClass}>Home</NavLink>
                     <NavLink to="/analytics" className={navClass}>Analytics</NavLink>
                     <NavLink to="/messages" className={navClass}>Messages</NavLink>
                     <NavLink to="/settings" className={navClass}>Settings</NavLink>
                     <NavLink to="/profile" className={navClass}>Profile</NavLink>
                 </Nav>
-                  <NavDropdown title={user.icon} id="basic-nav-dropdown" className="me-3">
-              
-              <NavDropdown.Item href="#action/3.2">
-                Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Settings
-              </NavDropdown.Item>
-              <NavDropdown.Item onClick={toggleTheme}>
-                Theme: {theme === 'dark' ? 'Dark' : 'Light'}
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavLink to="/messages" className='nav-link'><FaBell size={30}/></NavLink>
+                  <div className="d-flex gap-2 mx-auto m-lg-0 w-auto">
+                    <NavDropdown title={user.icon} id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.2">
+                        Profile
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Settings
+                      </NavDropdown.Item>
+                      <NavDropdown.Item onClick={toggleTheme}>
+                        Theme: {theme === 'dark' ? 'Dark' : 'Light'}
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action/3.4">
+                        Separated link
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavLink to="/messages" className='nav-link'><FaBell size={30}/></NavLink>
+                  </div>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
