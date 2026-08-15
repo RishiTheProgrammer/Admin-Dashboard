e671170872e9d6b4f0a29f6517a429be490e87d0
Rishi Soni <rishisprogramming@gmail.com>
2026-08-15 23:27:03 +0500

    Created folder to move landing page here

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <NavLink to="/home">Home</NavLink>
+    </div>
+  )
+}
+
+export default LandingPage
