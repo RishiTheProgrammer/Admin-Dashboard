2c62d1988ff02c2b33cf3522bd564a4359ee3431
Rishi Soni <rishisprogramming@gmail.com>
2026-08-15 16:12:29 +0500

    Added path for landing page and chanbed path style of home page

--- src/components/mainArea.jsx
+++ src/components/mainArea.jsx
@@ -1,5 +1,6 @@
 import { Col } from 'react-bootstrap'
 import { Routes, Route } from 'react-router-dom'
+import LandingPage from '../sections/LandingPage'
 import Home from '../sections/Home'
 import Analytics from '../sections/Analytics'
 import Messages from '../sections/Messages'
@@ -10,7 +11,8 @@ const MainArea = () => {
   return (
     <Col className='overflow-y-auto flex-grow-1' style={{marginTop: "90px"}}>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<LandingPage />}/>
+            <Route path="/home" element={<Home />} />
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/messages" element={<Messages />} />
             <Route path="/settings" element={<Settings />} />
