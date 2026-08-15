67501f370929e7b0ec9b01e31510fe4544e9b2e4
Rishi Soni <rishisprogramming@gmail.com>
2026-08-15 16:11:32 +0500

    Edited path of home page and some info dashboard

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { TbHexagonLetterRFilled } from "react-icons/tb";
 import { TbCircleLetterRFilled } from "react-icons/tb";
 import { FaBell } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 import { NavLink } from "react-router-dom";
 import './navbar.css'
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -19,6 +20,15 @@ const navbar = ({theme, setTheme}) => {
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   }
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    const confirm = window.confirm("Are you sure you want to log out?");
+
+    if(confirm){
+      localStorage.clear();
+      navigate("/");
+    }
+  }
   return (
     <>
         <Navbar expand="lg" className="bg-body-tertiary m-2 rounded-2 px-2 py-2 navbar" fixed="top">
@@ -28,7 +38,7 @@ const navbar = ({theme, setTheme}) => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
             <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-lg`}
+              id="offcanvasNavbar-expand-lg"
               aria-labelledby="offcanvasNavbarLabel-expand-lg"
               placement="end"
             >
@@ -39,29 +49,29 @@ const navbar = ({theme, setTheme}) => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="mx-lg-auto mx-0 d-lg-flex align-items-center gap-4">
-                    <NavLink to="/" end className={navClass}>Home</NavLink>
+                    <NavLink to="/home" end className={navClass}>Home</NavLink>
                     <NavLink to="/analytics" className={navClass}>Analytics</NavLink>
                     <NavLink to="/messages" className={navClass}>Messages</NavLink>
                     <NavLink to="/settings" className={navClass}>Settings</NavLink>
                     <NavLink to="/profile" className={navClass}>Profile</NavLink>
                 </Nav>
                   <div className="d-flex gap-2 mx-auto m-lg-0 w-auto">
-                    <NavDropdown title={user.icon} id="basic-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.2">
-                        Profile
+                    <NavDropdown title={user.icon} id="basic-nav-dropdown" className={`account`}>
+                      <NavDropdown.Item>
+                        <NavLink to="/profile" className='text-secondary text-decoration'>Profile</NavLink>
                       </NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.3">
-                        Settings
+                      <NavDropdown.Item>
+                        <NavLink to="/settings" className='text-secondary text-decoration'>Settings</NavLink>
                       </NavDropdown.Item>
                       <NavDropdown.Item onClick={toggleTheme}>
                         Theme: {theme === 'dark' ? 'Dark' : 'Light'}
                       </NavDropdown.Item>
                       <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action/3.4">
-                        Separated link
+                      <NavDropdown.Item>
+                        <button className='btn text-danger border-0' onClick={handleLogin}>Log out</button>
                       </NavDropdown.Item>
                     </NavDropdown>
-                    <NavLink to="/messages" className='nav-link'><FaBell size={30}/></NavLink>
+                    <NavLink to="/messages" className="py-2 px-2"><FaBell size={30}/></NavLink>
                   </div>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
