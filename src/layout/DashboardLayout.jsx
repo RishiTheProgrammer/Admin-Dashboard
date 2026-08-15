e18ad1e77af85da30c8bed1e2d194fd670119b52
Rishi Soni <rishisprogramming@gmail.com>
2026-08-15 23:26:36 +0500

    Created file to make layout and render landing page this layout

---
+++ src/layout/DashboardLayout.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import MainArea from '../components/mainArea'
+import Navbar from '../components/navbar'
+
+const DashboardLayout = () => {
+    const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "dark"
+  });
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', theme);
+
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+  return (
+    <div className='vh-100 d-flex flex-column'>
+          <Navbar theme={theme} setTheme={setTheme} />
+        <Container fluid>
+          <Row>
+            <MainArea />
+          </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default DashboardLayout
