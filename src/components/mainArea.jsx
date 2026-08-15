b15c59c113f5632a99b324ab0ec5c148196c49d7
Rishi Soni <rishisprogramming@gmail.com>
2026-08-15 23:25:14 +0500

    Removed landing page route path

--- src/components/mainArea.jsx
+++ src/components/mainArea.jsx
@@ -1,6 +1,5 @@
 import { Col } from 'react-bootstrap'
 import { Routes, Route } from 'react-router-dom'
-import LandingPage from '../sections/LandingPage'
 import Home from '../sections/Home'
 import Analytics from '../sections/Analytics'
 import Messages from '../sections/Messages'
@@ -11,12 +10,11 @@ const MainArea = () => {
   return (
     <Col className='overflow-y-auto flex-grow-1' style={{marginTop: "90px"}}>
         <Routes>
-            <Route path="/" element={<LandingPage />}/>
-            <Route path="/home" element={<Home />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="home" element={<Home />} />
+            <Route path="analytics" element={<Analytics />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="profile" element={<Profile />} />
         </Routes>
     </Col>
   )
