ccc2b2c159ca09a30990bb762978ab16d3e0f15e
Rishi Soni <rishisprogramming@gmail.com>
2026-08-22 16:42:45 +0500

    Created component for rendering multiple cards in Landing Page

---
+++ src/components/StatCard.jsx
@@ -0,0 +1,15 @@
+import { Card } from 'react-bootstrap'
+
+const StatCard = ({icon, title, description, CardBodyClass}) => {
+  return (
+    <Card className="bg-transparent p-3 rounded-3 border-3 text-white">
+        <Card.Body className={CardBodyClass}>
+            {icon}
+            <Card.Title className="fs-3">{title}</Card.Title>
+            <Card.Text className="text-secondary fw-semibold">{description}</Card.Text>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default StatCard
