c0972a82f50184b4943d5373ea8a87651d931675
Rishi Soni <rishisprogramming@gmail.com>
2026-08-23 15:47:03 +0500

    Created class for doing CSS

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,8 +1,9 @@
 import { Card } from 'react-bootstrap'
+import './statCard.css'
 
 const StatCard = ({icon, title, description, CardBodyClass}) => {
   return (
-    <Card className="bg-transparent p-3 rounded-3 border-3 border-secondary text-white">
+    <Card className="bg-transparent p-3 rounded-3 border-3 border-secondary text-white card">
         <Card.Body className={CardBodyClass}>
             {icon}
             <Card.Title className="fs-3">{title}</Card.Title>
